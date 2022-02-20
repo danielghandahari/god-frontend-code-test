43d8e5a32e1a80d6dee1199d05bd6f286cf4c71d
Daniel Ghandahari <dangha94@gmail.com>
2022-02-20 13:48:40 +0100

    horizontal car scroll

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import Image from "next/image";
+import NextLink from "next/link";
+import { Flex, Text, Card, CardContent, Spacer, Link } from "vcc-ui";
+
 import { useFakeFetchCars } from "../src/hooks/client";
 
 export default function Home() {
@@ -6,16 +10,65 @@ export default function Home() {
 
   switch (fetchedCars.status) {
     case "loading":
-      return <div>Loading...</div>;
+      return <Text>Loading...</Text>;
 
     case "success":
-      return fetchedCars.data.map((c) => (
-        <div key={c.id}>{`Car: ${c.id}`}</div>
-      ));
+      return (
+        <Flex
+          extend={{
+            border: "solid red",
+            flexDirection: "row",
+            overflowX: "auto",
+            gap: "1rem",
+          }}
+        >
+          {fetchedCars.data.map((c) => (
+            <div
+              key={c.id}
+              style={{
+                border: "solid blue",
+                minWidth: "25rem",
+                maxWidth: "25rem",
+              }}
+            >
+              <Card>
+                <CardContent>
+                  <Text extend={{ fontSize: "0.75rem" }}>{c.bodyType}</Text>
+                  <Spacer />
+                  <Text extend={{ fontWeight: "bold" }}>{c.modelName}</Text>
+                  <Spacer />
+                  <Text>{c.modelType}</Text>
+                  <Spacer />
+                  <Image
+                    alt="car image"
+                    src={c.imageUrl}
+                    width={1000}
+                    height={800}
+                  ></Image>
+                  <Flex
+                    extend={{
+                      flexDirection: "row",
+                      justifyContent: "center",
+                      gap: "2rem",
+                    }}
+                  >
+                    <NextLink href={`/learn/${c.id}`} passHref>
+                      <Link arrow="right">LEARN</Link>
+                    </NextLink>
+                    <NextLink href={`/shop/${c.id}`} passHref>
+                      <Link arrow="right">SHOP</Link>
+                    </NextLink>
+                  </Flex>
+                </CardContent>
+              </Card>
+            </div>
+          ))}
+        </Flex>
+      );
 
     case "error":
       return (
-        <p>{`Failed showing cars with error: ${fetchedCars.error.message}`}</p>
+        <Text>{`Failed showing cars with error: ${fetchedCars.error.message}`}</Text>
       );
 
     default:
